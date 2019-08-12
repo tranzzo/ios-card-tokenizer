259281686093a6e0336fb76f956336b4692b49dc
Vladislav Taiurskyi <v.taiurskyi@tranzzo.com>
2019-08-12 11:18:24 +0300

    Initial Commit

---
+++ widget-light-sdk/widget_light_sdk.h
@@ -0,0 +1,19 @@
+//
+//  widget_light_sdk.h
+//  widget-light-sdk
+//
+//  Created by Vladislav Taiurskyi on 8/12/19.
+//  Copyright © 2019 Tranzzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for widget_light_sdk.
+FOUNDATION_EXPORT double widget_light_sdkVersionNumber;
+
+//! Project version string for widget_light_sdk.
+FOUNDATION_EXPORT const unsigned char widget_light_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <widget_light_sdk/PublicHeader.h>
+
+
