1cee95c614f5fcc9ee1ad643f57a410ea4b8ecfc
Vladislav Taiurskyi <v.taiurskyi@tranzzo.com>
2019-08-27 14:10:14 +0300

    Fix access control via ABI Swift5 stability, rename SDK

---
+++ TranzzoSDK/TranzzoSDK.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) TRANZZO LTD - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TranzzoSDK.
+FOUNDATION_EXPORT double TranzzoSDKVersionNumber;
+
+//! Project version string for TranzzoSDK.
+FOUNDATION_EXPORT const unsigned char TranzzoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TranzzoSDK/PublicHeader.h>
+
+
