7fdeee626be89819388e2c076902a502134ad571
mmisesin <>
2020-01-14 11:02:33 +0200

    Update headers

--- TranzzoSDK/DataEncoder.swift
+++ TranzzoSDK/DataEncoder.swift
@@ -1,10 +1,8 @@
-//
-//  DataEncoder.swift
-//  TranzzoSDK
-//
-//  Created by user on 1/13/20.
-//  Copyright © 2020 Tranzzo. All rights reserved.
-//
+/*
+* Copyright (c) TRANZZO LTD - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+*/
 
 import Foundation
 
